0562e5f090fc7abe901cd1b46782ce6a8efa4dae
nhicas <jp99jp99@gmail.com>
2022-10-14 11:39:40 +0100

    parede 1

--- src/ex1.java
+++ src/ex1.java
@@ -1,2 +1,18 @@
 public class ex1 {
+    //parte 1
+    public static void main( String [] args) {
+        int i=100;
+        while (i<=200){
+            if (i<200){
+                System.out.println(i);
+                i++;
+            }else {
+                System.out.println(i);
+                System.out.println("\n\ndene!");
+                break;
+            }
+        }
+        //parde 2
+
+    }
 }
